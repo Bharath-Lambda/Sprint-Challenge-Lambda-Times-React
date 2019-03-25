f1ac8d167e1c04b82859d1f4bae998c4b210d836
Bharath <b.lavendra.95@gmail.com>
2019-03-25 18:39:46 -0500

    Rendering but not functional

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -6,7 +6,7 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {props.Tabs.map(tab => <Tab key = {tab} selectTabHandler = {props.selectTabHandler} selectedTab = {props.selectedTab} tab = {tab} />)
+        {props.tabs.map(tab => <Tab selectTabHandler = {props.selectTabHandler} selectedTab = {props.selectedTab} tab = {tab} />)
           /* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
@@ -15,4 +15,5 @@ const Tabs = props => {
 };
 
 // Make sure to use PropTypes to validate your types!
+
 export default Tabs;
